e97eae1248edd0f0bbd11eb8891561cee75053c7
Palathip Wuttiprayoon <boom.palathip@gmail.com>
2020-03-21 14:30:38 +0700

    edit router

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/article', '/articles', 'ArticleController@index');
-
-
+Route::resource('/article', 'ArticleController');
